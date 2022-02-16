55bed96e8a7451dafc7816d66c9d46bb53324f2d
Adrian Pajka <ad.pajka@gmail.com>
2022-02-16 15:27:38 +0100

    reference argument added

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ from src.world_bank import WorldBank
 
 def forecast_and_save(country: str, time_series_code: str, forecast_year):
     """Forecast tiem series from World bank API and save data to csv."""
-    filename:str = date.today().strftime("%b-%d-%Y")
-    
-    wb = WorldBank(time_series_code=time_series_code, country = country)
+    filename: str = date.today().strftime("%b-%d-%Y")
+
+    wb = WorldBank(time_series_code=time_series_code, country=country)
     ts = TimeSeries(wb)
-    arima = ArimaModel(ts) # you can change it to PbprophetModel if it's implemented with schema
-    f = Forecast(arima)
-    
+    # you can change it to PbprophetModel if it's implemented with schema
+    arima = ArimaModel(ts)
+    f = Forecast(arima, forecast_year)
+
     stages = ('historical', 'predicted', 'combined')
-    for stage, df in zip(stages,(f.historical_data, f.predicted_data, f.combined_df)):
-        print('Saving', stage,f' as {stage+filename}.csv')
+    for stage, df in zip(stages, (f.historical_data, f.predicted_data, f.combined_df)):
+        print('Saving', stage, f' as {stage+filename}.csv')
         save_data(df, stage+filename)
-        
 
 
 if __name__ == '__main__':
